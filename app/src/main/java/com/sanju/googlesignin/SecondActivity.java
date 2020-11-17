64e6d05f1baba25127ee472775f2167c11fd5f0c
kumar-sanju <mailmeinmymail0@gmail.com>
2020-11-17 17:12:51 +0530

    new one

---
+++ app/src/main/java/com/sanju/googlesignin/SecondActivity.java
@@ -0,0 +1,32 @@
+package com.sanju.googlesignin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SecondActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(SecondActivity.this,MainActivity.class));
+                finish();
+            }
+        });
+
+    }
+}
